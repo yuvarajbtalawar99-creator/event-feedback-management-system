c259cfbf09f01e3f597c5b968b166ac3d1c1af1e
yuvaraj b talawar <yuvarajbtalawar99@gmail.com>
2026-05-23 16:54:54 +0530

    Update MongoDB connection URI in promote_users.js

--- backend/promote_users.js
+++ backend/promote_users.js
@@ -16,7 +16,7 @@ const User = mongoose.model('User', userSchema);
 
 const promoteUsers = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://EventFeedback:Yuvaraj999@eventfeedback.wxyry8n.mongodb.net/?appName=Eventfeedback');
+    await mongoose.connect(process.env.MONGO_URI || 'URL');
     console.log('Connected to MongoDB.');
     
     // Update all users to admin role
